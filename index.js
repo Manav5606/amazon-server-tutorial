b1c6f83fbaa82b107cb57d48565e93e9e82093e5
Manav <manavpatel528@gmail.com>
2022-05-30 20:47:15 +0530

    uploading server

---
+++ index.js
@@ -0,0 +1,35 @@
+const e = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+const PORT = 3000;
+const app = express();
+const DB =
+  "mongodb+srv://manav:manav5606@cluster0.ehdrd.mongodb.net/?retryWrites=true&w=majority";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+
+//Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Connected at port ${PORT} `);
+});
